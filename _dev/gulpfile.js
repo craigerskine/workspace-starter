3e904a68cfbca4d7bdf1f89027fc659989430018
Craig Erskine <craig@craigerskine.com>
2020-02-04 15:25:54 -0600

    Update gulpfile.js

--- _dev/gulpfile.js
+++ _dev/gulpfile.js
@@ -17,16 +17,8 @@ gulp.task('css', function(){
   ]))
   .pipe(purgecss({
     content: ['app/master.xsl', '../live/workspace/theme/**/*.xsl', '../live/workspace/theme/assets/**/*.js'],
-    extractors: [
-      {
-        extractor: class TailwindExtractor {
-          static extract(content) {
-            return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-          }
-        },
-        extensions: ['xsl', 'js']
-      }
-    ]
+    defaultExtractor: content =>
+      content.match(/[\w-/:]+(?<!:)/g) || []
   }))
   .pipe(autoprefixer({
     cascade: false
